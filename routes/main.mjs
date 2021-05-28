2c3db661dce7808dbf907f4e219d5b98e9c4dfbe
Wei Jie <neoweijie1234@gmail.com>
2021-05-28 16:06:31 +0800

    fixed rotes, created models

--- routes/main.mjs
+++ routes/main.mjs
@@ -39,17 +39,5 @@ router.get("/about", async function(req, res) {
 	});
 });
 
-router.get("/feedback", async function(req, res) {
-	console.log("Feedback page accessed");
-	return res.render('../templates/feedback/feedback.html', {
-		title: "Hello World"
-	});
-});
 
-router.get("/viewFeedback", async function(req, res) {
-	console.log("View Feedback page accessed");
-	return res.render('../templates/feedback/viewFeedback.html', {
-		title: "Hello World"
-	});
-});
 
