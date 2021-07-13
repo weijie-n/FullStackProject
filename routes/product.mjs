99698ba5b002ca8ac9a35ee3bcc7fdad6cd634de
xin hui <xinhuix07@gmail.com>
2021-07-13 11:38:56 +0800

    hi

--- routes/product.mjs
+++ routes/product.mjs
@@ -2,10 +2,21 @@ import { Router } from 'express';
 const router = Router();
 export default router;
 
-router.get("/create",      async function(req, res) {
+router.get("/create",     product_page);
+router.post("/create",    product_process);
+
+/**
+ * Renders the product page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
+
+async function product_page (req, res) {
 	console.log("product page accessed");
 	return res.render('product/create.html');
-});
+}
+
+/*
 
 router.get("/view",      async function(req, res) {
 	console.log("Viewing product page accessed");
@@ -16,3 +27,5 @@ router.get("/update",      async function(req, res) {
 	console.log("Updating product page accessed");
 	return res.render('product/update.html');
 });
+
+*/
