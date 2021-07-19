1f583d6786136e6a556d7ea7a3f21cb6efc0c9bd
farah <farahnurliyana07@gmail.com>
2021-07-19 20:21:52 +0800

    orders routes

--- routes/orders.mjs
+++ routes/orders.mjs
@@ -1,23 +1,27 @@
 import { Router } from 'express';
-// import { ModelCart } from '../data/cart';
 const router = Router();
 export default router;
 
-router.get("/",      async function(req, res) {
-	console.log("Create Order page accessed");
-	return res.render('orders/createForm.html');
-});
+router.get("/create",     orders_page);
+router.post("/create",    orders_process);
 
-router.get("/view",      async function(req, res) {
-	console.log("View Order page accessed");
-	return res.render('orders/retrieveForm.html', {
-		products: [
-			{ product: null, qty: 0 }
-		]
-	});
-});
+/**
+ * Renders the product page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
 
+async function orders_page (req, res) {
+	console.log("Orders page accessed");
+	return res.render('orders/create.html');np
+}
 
-
-
-
+/**
+* Process the login form body
+ * @param {import('express').Request}  req Express Request handle
+ * @param {import('express').Response} res Express Response handle
+ */
+async function orders_process(req, res) {
+	console.log("orders contents received");
+	console.log(req.body);
+}
