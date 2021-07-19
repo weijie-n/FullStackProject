4a58e5f8a0120c3bccc4d784a0daf51eb19fcc29
farah <farahnurliyana07@gmail.com>
2021-07-19 21:45:16 +0800

    order and feedback page done!

--- routes/feedback.mjs
+++ routes/feedback.mjs
@@ -2,28 +2,26 @@ import { Router } from 'express';
 const router = Router();
 export default router;
 
+router.get("/create",     feedback_page);
+router.post("/create",    feedback_process);
 
+/**
+ * Renders the product page
+ * @param {Request}  req Express Request handle
+ * @param {Response} res Express Response handle
+ */
 
-///	After request
-
-//	HOME PAGE of Feedback
-router.get("/", async function(req, res) {
+async function feedback_page (req, res) {
 	console.log("Feedback page accessed");
-	return res.render('feedback/feedback.html', {
-		title: "Hello World"
-	});
-});
+	return res.render('feedback/create.html');
+}
 
-router.get("/create", async function(req, res) {
-	console.log("View Feedback page accessed");
-	return res.render('feedback/viewFeedback.html', {
-		title: "Hello World"
-	});
-});
-
-router.get("/edit", async function(req, res) {
-	console.log("View Feedback page accessed");
-	return res.render('feedback/viewFeedback.html', {
-		title: "Hello World"
-	});
-});
+/**
+* Process the login form body
+ * @param {import('express').Request}  req Express Request handle
+ * @param {import('express').Response} res Express Response handle
+ */
+async function feedback_process(req, res) {
+	console.log("Feedback contents received");
+	console.log(req.body);
+}
